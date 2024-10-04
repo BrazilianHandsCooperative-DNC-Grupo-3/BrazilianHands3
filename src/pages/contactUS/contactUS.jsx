d80128eb8039d741d418a818c6e2e3ec81ff8d23
lipebezerra95 <filipebza2019@gmail.com>
2024-10-03 23:11:43 -0300

    politica de privacidadade elaborada e estilizada!

--- src/pages/contactUS/contactUS.jsx
+++ src/pages/contactUS/contactUS.jsx
@@ -6,9 +6,26 @@ import NavBar from '../../components/navBar/navBar'
 const ContactUS = () => {
   return (
     <div>
+      
          <NavBar />
+         <section className='container'>
+        <section className='text-JoinOurTeam'>
+          <h1>
+          schedule your service <br />
+            <br />
+          </h1>
+          <p>
+            Work with us. Please provide your details below and describe <br />
+            the services you offer to join our team <br />
+            <br />
+            Join Brazilian Hands Cooperative: Where Your Skills Meet <br />
+            Opportunity!
+          </p>
+        </section>
+      </section>
+      
          
-        <ContactForm / >
+        <ContactForm />
     </div>
   )
 }
---
+++ src/pages/privacyPolicy/privacyPolicy.jsx
@@ -0,0 +1,70 @@
+import './index.scss'
+import NavBar from '../../components/navBar/navBar.jsx'
+
+const privacyPolicy = () => {
+  return (
+    <div>
+      <NavBar/>
+
+      <section className='container'>
+        <div className='policy'>
+        <h1 className='priv'>Política de Privacidade da BrazilianHands coperative</h1>
+        <h3 className='poli'>1. Introdução</h3>
+        <p className='spriv'>A sua privacidade é importante para nós. Esta Política de Privacidade descreve como coletamos,<br></br> usamos, armazenamos e protegemos suas informações pessoais em conformidade com a Lei Geral de Proteção de Dados (LGPD)<br/> e outras legislações aplicáveis. Ao acessar nosso site, você concorda com as práticas descritas nesta política.</p>
+        <h3 className='poli'>2. Coleta de Informações Pessoais</h3>
+        <p className='spriv'>Coletamos informações pessoais que você nos fornece diretamente,<br/> como:<br/>
+
+        Nome<br/>
+        Endereço de e-mail<br/>
+        Número de telefone<br/>
+        Endereço<br/>
+        Informações financeiras (quando aplicável)<br/>
+        Além disso, podemos coletar informações automaticamente, como:<br/>
+
+        Dados de navegação (endereços IP, páginas acessadas, horários de acesso)<br/>
+        Cookies e tecnologias semelhantes
+        </p>
+        <h3 className='poli'>3. Uso das Informações</h3>
+        <p className='spriv'>As informações coletadas são utilizadas para:
+
+        Coletar informações de clientes e cooperados <br/> para a utilização do serviço
+        Processar transações financeiras
+        Comunicar-se com você sobre nossos serviços<br/> e ofertas
+        Melhorar nosso site e a experiência do usuário
+        Cumprir obrigações legais e regulatórias
+        </p>
+        <h3 className='poli'>4. Compartilhamento de Informações</h3>
+        <p className='spriv'>Não vendemos ou alugamos suas informações pessoais a terceiros. Podemos compartilhar<br/> suas informações com:
+
+        Prestadores de serviços que nos ajudam a operar o site ou realizar atividades em nosso<br/> nome
+        Autoridades competentes quando exigido por lei</p>
+        <h3 className='poli'>5. Armazenamento e Segurança</h3>
+        <p className='spriv'>Suas informações pessoais são armazenadas em servidores seguros e protegidas por medidas de segurança apropriadas.<br/> Mantemos suas informações pelo tempo necessário para cumprir as finalidades descritas nesta política ou conforme exigido por lei.</p>
+        <h3 className='poli'>6. Seus Direitos</h3>
+        <p className='spriv'>Você tem o direito de:<br/>
+
+        Acessar suas informações pessoais<br/>
+        Corrigir dados incompletos ou imprecisos<br/>
+        Solicitar a exclusão de suas informações<br/>
+        Revogar o consentimento para o tratamento de seus dados
+        Para exercer esses direitos,<br/> entre em contato conosco através dos meios indicados na seção de contato.
+        </p>
+        <h3 className='poli'>7. Alterações na Política de Privacidade</h3>
+        <p className='spriv'>Reservamo-nos o direito de atualizar esta Política de Privacidade<br/> a qualquer momento. Alterações serão publicadas em nosso site com a data da última atualização.</p>
+        <h3 className='poli'>8. Contato</h3>
+        <p className='spriv'>Se você tiver dúvidas ou preocupações sobre esta Política de Privacidade ou sobre o tratamento de suas<br/> informações pessoais, entre em contato conosco:<br/>
+
+        E-mail: [seu-email@cooperativa.ie]<br/>
+        Telefone: [número de telefone]<br/>
+        Endereço: [endereço da cooperativa]</p>
+        <h3 className='poli'>9. Aceitação</h3>
+        <p className='spriv'>Ao utilizar nosso site, você aceita os termos desta Política de Privacidade.<br/> Se você não concordar, pedimos que não utilize nossos serviços.<br/>
+
+        Esta política foi elaborada com o intuito de garantir a transparência e a proteção dos dados pessoais de nossos<br/> usuários. Agradecemos pela confiança na Brazilian Hands Coperative!</p>
+        </div>
+      </section>
+    </div>
+  )
+}
+
+export default privacyPolicy
